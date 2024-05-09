5104d2254dbd308c0e62ffd6533c939b5f11b638
Ahany42 <ahanyfathy42@gmail.com>
2024-05-09 11:55:30 +0300

    Form 6 GUI

--- Magazine-System/Form6.Designer.cs
+++ Magazine-System/Form6.Designer.cs
@@ -31,38 +31,54 @@ namespace Magazine_System
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(158, 132);
+            this.button1.Location = new System.Drawing.Point(582, 170);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(307, 56);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Crystal reports 1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 128);
+            this.button2.Location = new System.Drawing.Point(582, 356);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 27);
+            this.button2.Size = new System.Drawing.Size(307, 56);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Crystal Reports 2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 50);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(246, 67);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Crystal reports";
             // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Magazine_System.Properties.Resources.HomeScreenBg;
+            this.ClientSize = new System.Drawing.Size(991, 595);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form6";
             this.Text = "Form6";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +86,6 @@ namespace Magazine_System
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
